a2010fc273895b2346dc94e77a4f31213299d1a8
rania260 <raniarezgui4@gmail.com>
2024-10-01 21:20:35 +0100

    login commit

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from './schemas/user.schema';
 import * as bcrypt from "bcryptjs";
 import { JwtService } from '@nestjs/jwt';
 import { SignUpDto } from './dto/signup.dto';
-
+import { LoginDto } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
@@ -14,8 +14,8 @@ export class AuthService {
         private userModel: Model<User>,
         private jwtService: JwtService,
     ) { }
-//logic registre
-    async signUp(signUpDto : SignUpDto): Promise<{ token: string }> {
+    //logic registre
+    async signUp(signUpDto: SignUpDto): Promise<{ token: string }> {
         const { username, email, password } = signUpDto;
         const hashedPassword = await bcrypt.hash(password, 10);
 
@@ -28,7 +28,22 @@ export class AuthService {
         const token = this.jwtService.sign(
             //payload: data li nheb naamllha save f token li heya =>id
             { id: user._id },
-            // { secret: this.configService.get<string>('JWT_SECRET') },
+        );
+        return { token };
+    }
+    //logic login
+    async login(loginDto: LoginDto): Promise<{ token: string }> {
+        const { email, password } = loginDto;
+        const user = await this.userModel.findOne({ email });
+        if (!user) {
+            throw new UnauthorizedException('Email is incorrect');
+        }
+        const isPasswordMatched = await bcrypt.compare(password, user.password);
+        if (!isPasswordMatched) {
+            throw new UnauthorizedException('Password is incorrect');
+        }
+        const token = this.jwtService.sign(
+            { id: user._id },
         );
         return { token };
     }
